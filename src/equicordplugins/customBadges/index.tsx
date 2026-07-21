4ce5004d843d6819b9c3412d207f482dac9fa9e9
Jahbas <kahba859@gmail.com>
2026-07-21 14:38:33 +0200

    fix: reverse badge order

--- src/equicordplugins/customBadges/index.tsx
+++ src/equicordplugins/customBadges/index.tsx
@@ -6,17 +6,9 @@ const USER_ID = "241671871509168129";
 
 const Badges: ProfileBadge[] = [
     {
-        id: "jahbas_look_right",
-        description: "Look Right",
-        iconSrc: "https://badge.equicord.org/badges/848339671629299742/6655581d88f729dde37bf0277ca95f4653f33e59.webp",
-        position: BadgePosition.START,
-        shouldShow: ({ userId }) => userId === USER_ID,
-        props: { style: { borderRadius: "50%", transform: "scale(0.9)" } },
-    },
-    {
-        id: "jahbas_around_world",
-        description: "Around the world",
-        iconSrc: "https://badges.vencord.dev/badges/848339671629299742/1-8d2f71fa61e73298c5f12644943d2ce59a22f6f7.gif",
+        id: "jahbas_contributor",
+        description: "Equicord Contributor",
+        iconSrc: "https://equicord.org/assets/favicon.png",
         position: BadgePosition.START,
         shouldShow: ({ userId }) => userId === USER_ID,
         props: { style: { borderRadius: "50%", transform: "scale(0.9)" } },
@@ -30,9 +22,17 @@ const Badges: ProfileBadge[] = [
         props: { style: { borderRadius: "50%", transform: "scale(0.9)" } },
     },
     {
-        id: "jahbas_contributor",
-        description: "Equicord Contributor",
-        iconSrc: "https://equicord.org/assets/favicon.png",
+        id: "jahbas_around_world",
+        description: "Around the world",
+        iconSrc: "https://badges.vencord.dev/badges/848339671629299742/1-8d2f71fa61e73298c5f12644943d2ce59a22f6f7.gif",
+        position: BadgePosition.START,
+        shouldShow: ({ userId }) => userId === USER_ID,
+        props: { style: { borderRadius: "50%", transform: "scale(0.9)" } },
+    },
+    {
+        id: "jahbas_look_right",
+        description: "Look Right",
+        iconSrc: "https://badge.equicord.org/badges/848339671629299742/6655581d88f729dde37bf0277ca95f4653f33e59.webp",
         position: BadgePosition.START,
         shouldShow: ({ userId }) => userId === USER_ID,
         props: { style: { borderRadius: "50%", transform: "scale(0.9)" } },
